86715b4026bbe0642d25401e4a4e79aae5ea0245
zhongjun <zhongjun@idss-cn.com>
2024-12-03 19:35:39 +0800

    init: init

---
+++ multipartfiles-type-validator-component/src/test/java/com/zhongjun/validator/filevalidator/controller/MultipartFileVerifyTestController.java
@@ -0,0 +1,29 @@
+package com.zhongjun.validator.filevalidator.controller;
+
+import com.zhongjun.validator.filevalidator.annotation.MultipartFileVerify;
+import com.zhongjun.validator.filevalidator.constant.FileType;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author hubin
+ * @date 2022/10/14
+ */
+@Slf4j
+@RestController
+@RequestMapping(
+        value = {"/accountService"},
+        produces = "application/json;charset=UTF-8")
+@RequiredArgsConstructor
+@Validated
+public class MultipartFileVerifyTestController {
+
+    @PostMapping("/testFileUpload")
+    public String testFileUpload(@Valid @MultipartFileVerify(value = {FileType.XLS, FileType.XLSX}, message = "请上传XLS、XLSX类型文件") @RequestParam("file") MultipartFile multipartFile) {
+        return "true";
+    }
+}
